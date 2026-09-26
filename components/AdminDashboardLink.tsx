a1ef1c3f59ea3c47eea36512fa62760cc8cf573a
Mikael <mikaelcain0085@gmail.com>
2026-09-26 15:05:47 +0530

    Polish admin dashboard navigation

--- components/AdminDashboardLink.tsx
+++ components/AdminDashboardLink.tsx
@@ -29,7 +29,7 @@ export default function AdminDashboardLink() {
           aria-label="Loading"
         />
       ) : (
-        "<- ADMIN DASHBOARD"
+        "ADMIN DASHBOARD"
       )}
     </Link>
   );
